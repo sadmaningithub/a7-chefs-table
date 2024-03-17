7646c13151dac43a9985032918f962b1cc08148a
sadmaningithub <sadmanahmed261@gmail.com>
2024-03-17 22:16:43 +0600

    nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-
-      <h1 className='text-6xl'>Chefs Table</h1>
-
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+    
+      <Header></Header>
 
     </>
   )
